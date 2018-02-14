d3c96d2410a8de91db1677f57f5553b8c01c41d3
Michelle Sturgill <isabelsturgill@comcast.net>
2018-02-13 20:47:00 -0800

    Form

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FormSubmission.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewBag.errors = new List<string>();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("submit")]
+        public IActionResult Success(string firstName, string lastName, int age, string email, string password)
+        {
+            User NewUser = new User
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Age = age,
+                Email = email,
+                Password = password
+            };
+
+            TryValidateModel(NewUser);
+            return View("Success");
+            
+        }
+
+    }
+}
